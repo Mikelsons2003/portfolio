363661a4a9987d72c8093c583266e161704a606d
Mikelsons2003 <ip19.m.mikelsons@vtdt.edu.lv>
2023-08-31 14:38:37 +0300

    main commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Portofilo in process...
-        </a>
-      </header>
-    </div>
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="bg-white p-6 rounded-lg shadow-md w-96">
+          <div className="text-center">
+            <h1 className="text-3xl font-semibold text-gray-800">Mike Tyson</h1>
+            <p className="text-sm text-gray-600">Web Developer</p>
+          </div>
+          <div className="mt-4">
+            <div className="mb-2">
+              <span className="text-gray-500">Email:</span>
+              <a href="mailto:miketyson@example.com" className="text-blue-600 ml-1">miketyson@example.com</a>
+            </div>
+            <div className="mb-2">
+              <span className="text-gray-500">GitHub:</span>
+              <a href="https://github.com/miketyson" className="text-blue-600 ml-1" target="_blank" rel="noopener noreferrer">github.com/miketyson</a>
+            </div>
+            <div className="mb-2">
+              <span className="text-gray-500">LinkedIn:</span>
+              <a href="https://www.linkedin.com/in/miketyson" className="text-blue-600 ml-1" target="_blank" rel="noopener noreferrer">linkedin.com/in/miketyson</a>
+            </div>
+          </div>
+          <div className="mt-6 text-center">
+            <p className="text-gray-700">Welcome to my portfolio. Here, you can find information about my projects and skills.</p>
+          </div>
+        </div>
+      </div>
   );
 }
 
